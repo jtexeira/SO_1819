7372327554f65e3934391cdf5dde566758e76544
MightyMime <jose.filipe.matos.ferreira@gmail.com>
2019-04-20 00:58:59 +0100

    ASCENDED MENDESS

--- generate.py
+++ generate.py
@@ -3,30 +3,30 @@ from string import ascii_lowercase
 from random import randint, choice
 
 def main():
-    id = 1
+    MAX_ID = 1
     n = 25000
     if len(sys.argv) > 1:
         n = int(sys.argv[1])
     for i in range(n):
         c = randint(0,2)
-        if c == 0:
-            print("i {0} {1}".format(
-                rString(),
-                rPrice()))
-            id += 1
-        elif c == 1:
-            print("n {0} {1}".format(
-                randint(1, id),
-                rString()))
-        elif c == 2:
-            print("p {0} {1}".format(
-                randint(1, id),
-                rPrice()))
+        v = [
+             lambda: "i {} {}".format(
+                 rString(),
+                 rPrice()),
+             lambda: "n {} {}".format(
+                 randint(1, MAX_ID),
+                 rString()),
+             lambda: "p {} {}".format(
+                 randint(1, MAX_ID),
+                 rPrice())
+            ]
+        print(v[c]())
+        if c is 0: MAX_ID += 1
 
 def rString(strLen=10):
     return ''.join(choice(ascii_lowercase) for i in range(strLen))
 
 def rPrice(priceMax=100000):
-    return str(randint(1, priceMax) / 10)
+    return randint(1, priceMax) / 10
 
 main()
